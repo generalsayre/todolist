abe74e65bca6a5aa0422afb133592bb6ba3e2e24
generalsayre <levi.is.dreaming@gmail.com>
2025-06-06 16:03:51 +0800

    adding scripts, the bones of this project - more js

---
+++ scripts.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('todo-form');
+const input = document.getElementById('todo-input');
+const list = document.getElementById('todo-list');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+const task = input.value.trim();
+if (task !== '') {
+    addTask(task);
+    input.value = '';
+    }
+});
+
+function addTask(taskText) {
+    const li = document.createElement('li');
+    li.textContent = taskText;
+}
+
+li.addEventListener('click', function () {
+    li.classList.toggle('completed');
+});
+
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'X';
+deleteBtn.addEventListener('click', function (e) {
+    e.stopPropagation();
+    list.removeChild(li);
+});
+
+li.apendChild(deleteBtn);
+list.appendChild(li);
