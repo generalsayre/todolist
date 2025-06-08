204f9ddd68e625d56da296fbda4039a7638ba262
generalsayre <levi.is.dreaming@gmail.com>
2025-06-08 12:37:24 +0800

    adding custom checkbox, checkmark

--- scripts.js
+++ scripts.js
@@ -12,15 +12,24 @@ function addTask() {
 
   const li = document.createElement("li");
 
+  const label = document.createElement("label");
+  label.classList.add("custom-checkbox");
+
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.classList.add("task-checkbox");
+
+  const customBox = document.createElement("span");
+  customBox.classList.add("checkmark");
+
+  label.appendChild(checkbox);
+  label.appendChild(customBox);
 
   const span = document.createElement("span");
   span.textContent = task;
   span.classList.add("task-text");
 
-  li.appendChild(checkbox);
+  li.appendChild(label);
   li.appendChild(span);
 
   checkbox.addEventListener('change', function () {
